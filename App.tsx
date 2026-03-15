8f8ebc4e93be99852b57d8a55ffda0122ec79726
Raghav67816 <kumaraghav079@gmail.com>
2026-03-15 15:40:58 +0530

    added base app

--- App.tsx
+++ App.tsx
@@ -1,44 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useEffect } from 'react';
+import { StyleSheet, PermissionsAndroid, View, Text } from 'react-native';
+import RNBluetoothClassic, {
+  BluetoothDevice
+} from 'react-native-bluetooth-classic';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+async function isBtEnabled() {
+  const isEnabled = await RNBluetoothClassic.isBluetoothEnabled();
+  if (isEnabled != null) {
+    if (!isEnabled) {
+      requestPerm();
+    }
+    else {
+      console.info("Bluetooth is already enabled")
+    }
+  }
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+async function requestPerm() {
+  const isGranted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT, {
+    title: "Enabled Bluetooth",
+    message: "Please enable bluetooth for data transmission",
+    buttonPositive: 'Allow',
+    buttonNegative: 'Deny'
+  })
 
+  if (isGranted) {
+    console.log("allowed")
+  }
+}
+
+function App() {
+
+  useEffect(() => {
+    const btInit = async () => {
+      isBtEnabled();
+    };
+
+    btInit();
+  }, [])
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black'
   },
 });
 
